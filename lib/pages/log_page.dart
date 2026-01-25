5872fb9e53c472656c6bf13863691f9985d44b01
Severus Melvin <melvinmulyono02@gmail.com>
2026-01-25 14:07:52 +0700

    enchance

--- lib/pages/log_page.dart
+++ lib/pages/log_page.dart
@@ -41,151 +41,155 @@ class _LogPageState extends State<LogPage> {
           icon: Icons.arrow_back,
         ),
       ),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 14),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const CustomSpacing(height: 16),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomText(
-                  text: 'Sign In',
-                  style: TextStyle(fontSize: 20, color: Colors.grey),
-                ),
-                CustomText(
-                  text: 'Fill in the blank',
-                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-            const CustomSpacing(height: 64),
-            CustomTextfield(
-              label: 'Email',
-              hint: '...@gmail.com',
-              useFlLabel: true,
-              isNumber: false,
-              controller: emailController,
-            ),
-            const CustomSpacing(height: 32),
-            Column(
-              children: [
-                CustomTextfield(
-                  label: 'Password',
-                  useFlLabel: true,
-                  isNumber: false,
-                  obscureText: obscureText,
-                  useSuffixIcon: true,
-                  suffixIcon: Icon(
-                    obscureText
-                        ? Icons.visibility_off_rounded
-                        : Icons.visibility,
-                    color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomSpacing(height: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomText(
+                    text: 'Sign In',
+                    style: TextStyle(fontSize: 20, color: Colors.grey),
                   ),
-                  onTapSuffixIcon: () {
-                    setState(() => isObscure());
-                  },
-                  controller: passwordController,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        CustomCheckbox(
-                          value: checked,
-                          onChanged: (value) {
-                            setState(() {
-                              checkBoxState();
-                            });
-                          },
-                          borderColor: Colors.grey,
-                        ),
-                        CustomText(
-                          text: 'Remember me',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                    CustomText(
-                      text: 'Forgot Password',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const CustomSpacing(height: 84),
-            Column(
-              children: [
-                CustomButton(
-                  onPressed: () {
-                    if (emailController.text.isEmpty ||
-                        passwordController.text.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: CustomText(text: "Please fill in the field"),
-                          duration: Duration(milliseconds: 800),
-                        ),
-                      );
-                      return;
-                    }
-                    if (emailController.text == "user@gmail.com" &&
-                        passwordController.text == "12345678") {
-                      Navigator.pushNamed(context, AppRoutes.homePage);
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: CustomText(
-                            text: "Please check your fill in",
-                          ),
-                          duration: Duration(milliseconds: 800),
-                        ),
-                      );
-                    }
-                  },
-                  text: 'Sign In',
-                  backgroundColor: PColor.primary,
-                  foregroundColor: SColor.white,
-                ),
-              ],
-            ),
-            const CustomSpacing(height: 32),
-            Row(
-              children: [
-                Expanded(child: Divider(color: Colors.grey)),
-                Expanded(
-                  flex: 2,
-                  child: CustomText(
-                    text: 'Or sign in with',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
+                  CustomText(
+                    text: 'Fill in the blank',
+                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              const CustomSpacing(height: 64),
+              CustomTextfield(
+                label: 'Email',
+                hint: '...@gmail.com',
+                useFlLabel: true,
+                isNumber: false,
+                controller: emailController,
+              ),
+              const CustomSpacing(height: 32),
+              Column(
+                children: [
+                  CustomTextfield(
+                    label: 'Password',
+                    useFlLabel: true,
+                    isNumber: false,
+                    obscureText: obscureText,
+                    useSuffixIcon: true,
+                    suffixIcon: Icon(
+                      obscureText
+                          ? Icons.visibility_off_rounded
+                          : Icons.visibility,
                       color: Colors.grey,
                     ),
+                    onTapSuffixIcon: () {
+                      setState(() => isObscure());
+                    },
+                    controller: passwordController,
                   ),
-                ),
-                Expanded(child: Divider(color: Colors.grey)),
-              ],
-            ),
-            const CustomSpacing(height: 64),
-            Row(
-              children: [
-                Expanded(
-                  child: CustomSocialbutton(type: 'facebook', onTap: () {}),
-                ),
-                const CustomSpacing(width: 16),
-                Expanded(
-                  child: CustomSocialbutton(type: 'google', onTap: () {}),
-                ),
-                const CustomSpacing(width: 16),
-                Expanded(
-                  child: CustomSocialbutton(type: 'apple', onTap: () {}),
-                ),
-              ],
-            ),
-          ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          CustomCheckbox(
+                            value: checked,
+                            onChanged: (value) {
+                              setState(() {
+                                checkBoxState();
+                              });
+                            },
+                            borderColor: Colors.grey,
+                          ),
+                          CustomText(
+                            text: 'Remember me',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      CustomText(
+                        text: 'Forgot Password',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const CustomSpacing(height: 84),
+              Column(
+                children: [
+                  CustomButton(
+                    onPressed: () {
+                      if (emailController.text.isEmpty ||
+                          passwordController.text.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: CustomText(
+                              text: "Please fill in the field",
+                            ),
+                            duration: Duration(milliseconds: 800),
+                          ),
+                        );
+                        return;
+                      }
+                      if (emailController.text == "user@gmail.com" &&
+                          passwordController.text == "12345678") {
+                        Navigator.pushNamed(context, AppRoutes.homePage);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: CustomText(
+                              text: "Please check your fill in",
+                            ),
+                            duration: Duration(milliseconds: 800),
+                          ),
+                        );
+                      }
+                    },
+                    text: 'Sign In',
+                    backgroundColor: PColor.primary,
+                    foregroundColor: SColor.white,
+                  ),
+                ],
+              ),
+              const CustomSpacing(height: 32),
+              Row(
+                children: [
+                  Expanded(child: Divider(color: Colors.grey)),
+                  Expanded(
+                    flex: 2,
+                    child: CustomText(
+                      text: 'Or sign in with',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  Expanded(child: Divider(color: Colors.grey)),
+                ],
+              ),
+              const CustomSpacing(height: 64),
+              Row(
+                children: [
+                  Expanded(
+                    child: CustomSocialbutton(type: 'facebook', onTap: () {}),
+                  ),
+                  const CustomSpacing(width: 16),
+                  Expanded(
+                    child: CustomSocialbutton(type: 'google', onTap: () {}),
+                  ),
+                  const CustomSpacing(width: 16),
+                  Expanded(
+                    child: CustomSocialbutton(type: 'apple', onTap: () {}),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
